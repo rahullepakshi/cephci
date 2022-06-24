809754cd2a2e8ff378c21bd4c58f4982050e3743
rlepaksh <rlepaksh@redhat.com>
2022-06-24 13:50:02 +0530

    Adding test case for CEPH-83573766
    Signed-off-by: rlepaksh <rlepaksh@redhat.com>

--- ceph/ceph_admin/osd.py
+++ ceph/ceph_admin/osd.py
@@ -178,6 +178,8 @@ class OSD(ApplyMixin, Orch):
         base_cmd.append("rm")
         osd_id = config["pos_args"][0]
         base_cmd.append(str(osd_id))
+        if config.get("args"):
+            base_cmd.append(config["args"][0])
         self.shell(args=base_cmd)
 
         check_osd_id_dict = {
